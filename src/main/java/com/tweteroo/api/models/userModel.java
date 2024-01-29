ddf7fd6e16f4ebc759162f7f50d484254e146979
montenegroleticia <montenegro.letiicia@gmail.com>
2024-01-29 14:02:54 -0300

    feat: user model

---
+++ src/main/java/com/tweteroo/api/models/userModel.java
@@ -0,0 +1,30 @@
+package com.tweteroo.api.models;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(length = 255, nullable = false)
+    private String avatar;
+
+    @Column(length = 100, nullable = false)
+    private String username;
+}
